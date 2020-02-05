69e8f43a643aa1448a370a93dd34f3aa8de555ed
Sami Leinonen <soleinonen@gmail.com>
2020-02-05 20:56:30 +0200

    Exercises 1-3 done. Added folder for week 2 exercises.

---
+++ viikko2/tehtavat1-3/src/main/java/ohtu/Multiplier.java
@@ -0,0 +1,12 @@
+package main.java.ohtu;
+
+public class Multiplier {
+    private int value;
+    public Multiplier(int value) {
+        this.value = value;
+    }
+
+    public int multipliedBy(int other) {
+        return value * other;
+    }
+}
---
+++ viikko2/tehtavat1-3/src/test/java/ohtu/MultiplierTest.java
@@ -0,0 +1,17 @@
+package test.java.ohtu;
+
+import static org.junit.Assert.*;
+import org.junit.Test;
+
+import main.java.ohtu.Multiplier;
+
+public class MultiplierTest {
+
+    @Test
+    public void kertominenToimii() {
+        Multiplier viisi = new Multiplier(5);
+
+        assertEquals(5, viisi.multipliedBy(1));
+        assertEquals(35, viisi.multipliedBy(7));
+    }
+}
