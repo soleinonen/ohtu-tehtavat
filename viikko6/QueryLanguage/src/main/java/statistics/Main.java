b670a137ec71384776ccb1933d021b79e070a98c
Sami Leinonen <soleinonen@gmail.com>
2020-03-28 20:07:59 +0200

    Viikko 6 tehtava 3.

--- viikko6/QueryLanguage/src/main/java/statistics/Main.java
+++ viikko6/QueryLanguage/src/main/java/statistics/Main.java
@@ -10,23 +10,14 @@ public class Main {
         // "https://nhlstatisticsforohtu.herokuapp.com/players.txt"
 
         Statistics stats = new Statistics(new PlayerReaderImpl(url));
-        /*
-        Matcher m = new And( new HasAtLeast(5, "goals"),
-                             new HasAtLeast(5, "assists"),
-                             new PlaysIn("PHI")
-        );
-        */
         
-        /*
-        Matcher m = new And( 
-        	    new HasFewerThan(1, "goals"), 
-        	    new PlaysIn("NYR")
-        	);
-		*/
-        
-        Matcher m = new And( 
-        	    new Not( new HasAtLeast(1, "goals") ), 
-        	    new PlaysIn("NYR")
+        Matcher m = new And(
+        	    new HasAtLeast(20, "points"),
+        	    new Or( 
+        	        new PlaysIn("NYR"),
+        	        new PlaysIn("NYI"),
+        	        new PlaysIn("NJD")
+        	    )
         	);
 
         
